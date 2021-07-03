a232d95e27c6bbdeae5fd98ad435ddc41a787e3c
Gyadav1718 <86192597+Gyadav1718@users.noreply.github.com>
2021-07-03 21:16:10 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,32 +1,35 @@
-var fixedRect,movingRect;
+var wall,car;
+var speed,weight;
+
 function setup() {
   createCanvas(800,400);
-  fixedRect= createSprite(200, 200, 50, 80);
-  movingRect= createSprite(400, 200, 80, 30);
+ 
+car = createSprite(50,200,50,50);
+
+wall = createSprite(700,200,60,height/2);
+wall.shapeColor= "blue";
+speed = random(50,120);
+weight = random(400,1500);
+car.velocityX= speed;
+
 
 }
 
 function draw() {
-  background(255,255,255);  
-  movingRect.x=World.mouseX;
-  movingRect.y=World.mouseY;
-if((movingRect.x-fixedRect.x<=fixedRect.width/2+movingRect.width/2 )&&fixedRect.x-movingRect.x<=fixedRect.width/2+movingRect.width/2)
-  {movingRect.shapeColor="red";
-  fixedRect.shapeColor="red";
-}
-else{movingRect.shapeColor="green";
-
-fixedRect.shapeColor="green";
-}
-if ((movingRect.y-fixedRect.y<=fixedRect.height/2+movingRect.height/2)&&fixedRect.y-movingRect.y<=fixedRect.height/2+movingRect.height/2){
-  movingRect.shapeColor="yellow";
-  fixedRect.shapeColor="blue";
-}
-else{
-  movingRect.shapeColor="green";
-fixedRect.shapeColor="green";
-}
-
-
+  background(255,255,255); 
+  if(wall.x-car.x < (car.width+wall.width)/2){
+    car.velocityX = 0;
+    var deformation = 0.5 * weight * speed* speed/22500;
+    }
+    if(deformation>180){
+      car.shapeColor = "red";
+    }
+    if(deformation<180 && deformation>100){
+      car.shapeColor = "yellow";
+    }
+    if(deformation<100){
+      car.shapeColor = "green";
+    }
+     
   drawSprites();
 }
